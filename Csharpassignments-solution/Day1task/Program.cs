ad0b9343773a544060d7f2d428df08eeb1a12e1e
Bhagavath-nsm <ramanathkaranns1999@gmail.com>
2025-05-13 10:37:53 +0530

    Add files via upload

---
+++ Csharpassignments-solution/Day1task/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day1task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("enter the number:");
+            string number=Console.ReadLine();
+            Console.Write("enter the width of the triangle:");
+            int width = int.Parse(Console.ReadLine());
+            int i = 1;
+            for (i = 1; i <= width; i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    Console.Write(number);
+                    
+                }
+                Console.WriteLine("\n");
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
